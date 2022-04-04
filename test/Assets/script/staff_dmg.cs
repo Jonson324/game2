8c2a02be85658487474c929ec78ee44d53d5ee39
IvanKrasawin <ivancrasawin@yandex.ru>
2022-04-04 21:42:05 +0300

    корректировка

--- test/Assets/script/staff_dmg.cs
+++ test/Assets/script/staff_dmg.cs
@@ -6,7 +6,7 @@ public class staff_dmg : MonoBehaviour
 {
 
     public Transform bullet;
-    public float bulletSpeed = 1000;
+    public float bulletSpeed = 10;
     public AudioClip Fire;
 
 
@@ -19,7 +19,7 @@ public class staff_dmg : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetKeyDown(KeyCode.Mouse0))
         {
             Transform bulletInstance = (Transform)Instantiate(bullet, GameObject.Find("spawn").transform.position, Quaternion.identity);
             bulletInstance.GetComponent<Rigidbody>().AddForce(transform.forward * bulletSpeed);
