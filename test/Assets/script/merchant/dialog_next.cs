168a580860e7caefa562a9be8507eb094051d9a1
observer <ananas.goroshkin@mail.ru>
2022-04-06 21:48:02 +0300

    Пробный торговец
    куб

---
+++ test/Assets/script/merchant/dialog_next.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dialog_next : MonoBehaviour
+{
+    public GameObject Text1, Text2;
+    private bool isText = true;
+    public task npc_taskScript;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown (0)) {
+            if (isText == true) {
+                isText = false;
+            } else {
+                isText = true;
+                npc_taskScript.EndDialog = true;
+            }
+        }
+        if (isText = true) {
+            Text1.SetActive(true);
+            Text2.SetActive(false);
+        } else {
+            Text1.SetActive(false);
+            Text2.SetActive(true);
+        }
+    }
+}
---
+++ test/Assets/script/merchant/task.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class task : MonoBehaviour
+{
+    public bool EndDialog;
+    public GameObject dialog1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (EndDialog == true) {
+            Time.timeScale = 1;
+            dialog1.SetActive(false); 
+        }
+    }
+    void OnTriggerEnter (Collider col) {
+        if (col.tag == "Player") {
+            Time.timeScale = 0; //pause
+            dialog1.SetActive(true); 
+        }
+    }
+}
