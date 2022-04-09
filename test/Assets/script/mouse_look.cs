49e0ecd15bffe43cd90862c119521949be1a48fe
IvanKrasawin <ivancrasawin@yandex.ru>
2022-04-09 11:37:12 +0300

    menu pause

--- test/Assets/script/mouse_look.cs
+++ test/Assets/script/mouse_look.cs
@@ -10,7 +10,7 @@ public class mouse_look : MonoBehaviour
 
     void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked;
+       // Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
